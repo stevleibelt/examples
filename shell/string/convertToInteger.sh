e21a26b14f432615f9c5579b5495844b3e1e1e37
stevleibelt <stev@leibelt.de>
2014-03-18 14:25:54 +0100

    added convert to string

---
+++ shell/string/convertToInteger.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+####
+# @author stev leibelt <artodeto@bazzline.net>
+# @since 2014-03-18
+####
+
+STRING="13"
+INTEGER=29
+RESULT=$((STRING+INTEGER))
+
+echo $STRING' + '$INTEGER' = '$RESULT
