f7559c004cbbb9bcd39ca5043788cd60af6c82af
stevleibelt <stev@leibelt.de>
2014-03-22 00:39:02 +0100

    improved version

--- shell/file/getContentOfColumn.sh
+++ shell/file/getContentOfColumn.sh
@@ -11,4 +11,11 @@ fi
 
 head -$2 $1 | tail -1   # seek to line $2 in file $1 (with head) and output this one column (with tial)
 
+##
+# cooler version, faster since we have to read the file once - good for large files
+#
+# TMPFILE='/tmp/tmpfile_'$0
+# head -$2 $1 | tail -1 | tee $TMPFILE   # seek to line $2 in file $1 (with head) and output which is catched by tee and redirects output to tail to read this one column
+# rm -f $TMPFILE
+
 exit 0
