4cb245b5a64c598212eb53d39e3ec106e9439ed5
stevleibelt <stev@leibelt.de>
2014-03-22 23:59:06 +0100

    rearranged scripts from howto repro

---
+++ shell/directory/basedir.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+########
+# base directory of script
+#
+# @author stev leibelt <artodeto@bazzline.net>
+# @since 2012-11-26
+########
+
+DIR_SELF=$(cd $(dirname "$0"); pwd)
+
+echo "Current working directory: $DIR_SELF"
--- shell/string/substring.sh
+++ shell/string/substring.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 ####
 # @author stev leibelt <artodeto@bazzline.net>
-# @since 2014-03-17
+# @since 2014-03-07
+#
+# links:
+#    * http://www.tldp.org/LDP/abs/html/string-manipulation.html
+#    * http://stackoverflow.com/questions/15351423/how-to-get-part-of-the-string-in-bash
+#    * https://www.linuxquestions.org/questions/linux-newbie-8/simple-bash-script-help-grabbing-part-of-a-string-316917/
 ####
 
-STRING="There is no foo without a bar"
-# ${<string>:<position>[:<length>]}
-SUBSTRING=${STRING:12:3}${STRING:26}
+STRING='FooBar-1.2.3-baz.tar.gz';
 
-echo $STRING
-echo $SUBSTRING
+#substringing with positions
+WITHOUT_FOO=${STRING:3};
+ONLY_NUMBERS=${STRING:7:5};
+
+echo 'string is: '$STRING;
+echo 'without Foo: '$WITHOUT_FOO;
+echo 'only numbers: '$ONLY_NUMBERS;
