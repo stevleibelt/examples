138ab728b1da93e18425c8d8fbdc416db5449d62
stevleibelt <stev@leibelt.de>
2014-04-20 21:03:26 +0200

    started date section

---
+++ php/date/dateTime.0000-00-00.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2014-04-20
+ */
+
+date_default_timezone_set('Europe/Berlin');
+
+$dateTime = new DateTime('0000-00-00');
+
+echo 'format(\'Y-md H:i:s\'): ' . $dateTime->format('Y-m-d H:i:s') . PHP_EOL;
+echo 'format(\'U\'): ' . $dateTime->format('U') . PHP_EOL;
