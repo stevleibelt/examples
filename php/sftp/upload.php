5655652bce138a3c9dfbb743bd5b9a10fa7d52bb
stevleibelt <stev@leibelt.de>
2014-05-19 17:26:30 +0200

    started sftp section with upload example

---
+++ php/sftp/upload.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2014-05-19
+ * see:
+ *      http://stackoverflow.com/questions/4689540/how-to-sftp-with-php
+ *      http://php.net/manual/en/function.ssh2-sftp.php
+ *      http://www.php.net/manual/en/function.ssh2-connect.php
+ *      http://www.php.net/manual/en/function.ssh2-scp-send.php
+ *      http://www.sitepoint.com/using-ssh-and-sftp-with-php/
+ */
+
+$localFilePath = '/tmp/sent_file'
+$password = '<password>';
+$port = 22;
+$remoteFilePath = '/tmp/received_file'
+$username = '<username>';
+$url = '<url>';
+
+$session = ssh2_connect($url, $port);
+$isAuthenticated = ssh2_auth_password($session, $username, $password);
+
+if ($isAuthenticated) {
+    ssh2_scp_send($session, $localFilePath, $remoteFilePath, 0644)
+} else {
+    echo 'authentification failed' . PHP_EOL;
+}
