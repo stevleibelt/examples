28a4ee274d97fcd728a9c0d85aa23cc0faaeddb0
stevleibelt <stev@leibelt.de>
2014-05-19 10:28:27 +0200

    added example for here doc

---
+++ php/string/hereDoc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2014-05-19
+ */
+
+$hereDoc = <<<YOURLIMITER
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+anim id est laborum.
+YOURLIMITER;
+
+echo $hereDoc . PHP_EOL;
