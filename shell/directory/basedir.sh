64f00fc065f493e835c716adfa947f9b9bd329a8
stevleibelt <stev@leibelt.de>
2014-05-23 10:30:35 +0200

    added second example

--- shell/directory/basedir.sh
+++ shell/directory/basedir.sh
@@ -6,7 +6,11 @@
 # @since 2012-11-26
 ########
 
+## get path of the called script
 SCRIPT_PATH=$(cd $(dirname "$0"); pwd)
+
+## get path of the current script
+SCRIPT_PATH=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
 
 echo "$0 is located in path $SCRIPT_PATH"
 exit 0
