595e3cf1f8bf21264cd072beccaa47a4395d4e44
stevleibelt <stev@leibelt.de>
2014-06-05 23:22:01 +0200

    extended example

--- php/string/hereDoc.php
+++ php/string/hereDoc.php
@@ -4,6 +4,8 @@
  * @since 2014-05-19
  */
 
+$variable = 'foo';
+
 $hereDoc = <<<YOURLIMITER
 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
@@ -11,6 +13,20 @@ exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
 dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
 anim id est laborum.
+\$variable
+YOURLIMITER;
+
+echo $hereDoc . PHP_EOL;
+echo PHP_EOL;
+
+$hereDoc = <<<YOURLIMITER
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+anim id est laborum.
+$variable
 YOURLIMITER;
 
 echo $hereDoc . PHP_EOL;
