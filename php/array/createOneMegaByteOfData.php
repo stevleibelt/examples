318e4d680e1fc7370d4b87d067a2025c294b6491
stevleibelt <stev@leibelt.de>
2014-07-23 22:25:02 +0200

    added example how to create one mega byte of usage

---
+++ php/array/createOneMegaByteOfData.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* @author stev leibelt <artodeto@bazzline.net>
+* @since 2014-07-23
+*/
+
+$anotherRound = true;
+$data = array();
+$initialMemoryUsage = memory_get_usage(true);
+$iterator = 0;
+$memoryUsageInMegaByteToGenerate = 4;
+
+while ($anotherRound) {
+    $data[] = $iterator;
+    $currentMemoryUsage = (memory_get_usage(true) - $initialMemoryUsage);
+    $currentMemoryUsageInMegaBytes = ($currentMemoryUsage / 1024 / 1024);
+    if ($currentMemoryUsageInMegaBytes === $memoryUsageInMegaByteToGenerate) {
+        $anotherRound = false;
+    }
+    ++$iterator;
+}
+
+echo 'done after ' . $iterator . ' steps' . PHP_EOL;
