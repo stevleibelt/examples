6187a4c0c8c95d34750754b3bd94e8b12b7132f4
stevleibelt <stev@leibelt.de>
2014-08-01 11:11:07 +0200

    added example for directory

--- php/filesystem/dirname.php
+++ php/filesystem/dirname.php
@@ -8,3 +8,8 @@ $file = __FILE__;
 
 echo 'file: ' . $file . PHP_EOL;
 echo 'dirname: ' . dirname($file) . PHP_EOL;
+
+$directory = __DIR__;
+
+echo 'directory: ' . $directory . PHP_EOL;
+echo 'dirname: ' . dirname($directory) . PHP_EOL;
