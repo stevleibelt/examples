f4de7dc5689c68808ef4a6fae6bfaa2f27d3cd45
stevleibelt <stev@leibelt.de>
2014-08-04 13:42:16 +0200

    added description

--- php/cli/signal.php
+++ php/cli/signal.php
@@ -19,23 +19,23 @@ class Example
 
         declare(ticks = 10);
 
-        pcntl_signal(SIGHUP,    array($this, 'signalHandler'));
-        pcntl_signal(SIGINT,    array($this, 'signalHandler'));
-        pcntl_signal(SIGUSR1,   array($this, 'signalHandler'));
-        pcntl_signal(SIGUSR2,   array($this, 'signalHandler'));
-        pcntl_signal(SIGQUIT,   array($this, 'signalHandler'));
-        pcntl_signal(SIGILL,    array($this, 'signalHandler'));
-        pcntl_signal(SIGABRT,   array($this, 'signalHandler'));
-        pcntl_signal(SIGFPE,    array($this, 'signalHandler'));
-        pcntl_signal(SIGSEGV,   array($this, 'signalHandler'));
-        pcntl_signal(SIGPIPE,   array($this, 'signalHandler'));
-        pcntl_signal(SIGALRM,   array($this, 'signalHandler'));
-        pcntl_signal(SIGTERM,   array($this, 'signalHandler'));
-        pcntl_signal(SIGCHLD,   array($this, 'signalHandler'));
-        pcntl_signal(SIGCONT,   array($this, 'signalHandler'));
-        pcntl_signal(SIGTSTP,   array($this, 'signalHandler'));
-        pcntl_signal(SIGTTIN,   array($this, 'signalHandler'));
-        pcntl_signal(SIGTTOU,   array($this, 'signalHandler'));
+        pcntl_signal(SIGHUP,    array($this, 'signalHandler'));    //controlling terminal is closed
+        pcntl_signal(SIGINT,    array($this, 'signalHandler'));  //interrupt this processing | ctrl+c
+        pcntl_signal(SIGUSR1,   array($this, 'signalHandler'));    //user defined conditions
+        pcntl_signal(SIGUSR2,   array($this, 'signalHandler'));    //user defined conditions
+        pcntl_signal(SIGQUIT,   array($this, 'signalHandler'));    //quit your processing
+        pcntl_signal(SIGILL,    array($this, 'signalHandler'));    //illegal instruction performed
+        pcntl_signal(SIGABRT,   array($this, 'signalHandler'));    //abort process
+        pcntl_signal(SIGFPE,    array($this, 'signalHandler'));    //error on arithmetic
+        pcntl_signal(SIGSEGV,   array($this, 'signalHandler'));    //invalid virtual memory reference
+        pcntl_signal(SIGPIPE,   array($this, 'signalHandler'));    //write to a pipe without other process is connected to it
+        pcntl_signal(SIGALRM,   array($this, 'signalHandler'));    //some kind of limit is reached
+        pcntl_signal(SIGTERM,   array($this, 'signalHandler'));  //termination signal | kill <pid>
+        pcntl_signal(SIGCHLD,   array($this, 'signalHandler'));    //child is terminated
+        pcntl_signal(SIGCONT,   array($this, 'signalHandler'));    //continue your work
+        pcntl_signal(SIGTSTP,   array($this, 'signalHandler'));    //terminal stop signal
+        pcntl_signal(SIGTTIN,   array($this, 'signalHandler'));    //background process attempting read
+        pcntl_signal(SIGTTOU,   array($this, 'signalHandler'));    //background process attempting write
         //... add all you need
  
         shuffle($this->items);
