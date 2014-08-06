46ac8706f056fa9f13343bbcd87c2715a966bbbf
stevleibelt <stev@leibelt.de>
2014-08-06 18:00:24 +0200

    added example for preg_replace

---
+++ php/string/preg_replace.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2014-08-06
+ */
+
+$subject = 'a12345-dasd-231';
+$patterns = array(
+    '/a\d{5}/',
+    '/^a\d{5}/',
+    '/^a\d{8}/',
+    '/$a\d{5}/',
+    '/\w/',
+    '/\d/'
+);
+$replacement = 'foo';
+
+echo 'subject: ' . $subject . PHP_EOL;
+
+foreach ($patterns as $pattern) {
+    echo 'pattern: ' . $pattern . PHP_EOL;
+
+    $matches = array();
+    $modifiedSubject = preg_replace($pattern, $replacement, $subject);
+
+    echo 'modified subject: ' . var_export($modifiedSubject, true) . PHP_EOL;
+}
