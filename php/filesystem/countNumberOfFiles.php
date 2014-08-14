bc7a6717b4d46014694d2a8ae3dc02bc1cb5f9ea
stevleibelt <stev@leibelt.de>
2014-08-14 16:00:32 +0200

    added example how to count number of files

---
+++ php/filesystem/countNumberOfFiles.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2014-08-14
+ */
+
+//find number of *.php files in directory
+$command = 'ls ' . __DIR__ . ' | grep .php$ | wc | awk \'{print $1}\'';
+$numberOfFiles = array();
+$return = null;
+exec($command, $numberOfFiles, $return);
+
+if ($return > 0) {
+    throw new Exception(
+        'following command created an error: "' . $command . '"' . PHP_EOL .
+        'return: "' . $return . '"'
+    );
+}
+
+echo var_export($numberOfFiles) . PHP_EOL;
