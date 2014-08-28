d402b77eacdb3c1315019729384996c3d7c7708a
stevleibelt <stev@leibelt.de>
2014-08-28 12:52:53 +0200

    added "unhandled arguments" and getters for all

--- php/cli/input.php
+++ php/cli/input.php
@@ -6,10 +6,17 @@
 
 require_once 'Input.php';
 
+$filePath = array_shift($argv);
 $input = new Input(new String(), $argv);
 
 echo 'number of arguments: ' . $input->getNumberOfArguments() . PHP_EOL;
-echo 'arguments: ' . var_export($input->getArguments(), true) . PHP_EOL;
+echo 'parameters: ' . var_export($input->getParameters(), true) . PHP_EOL;
+echo 'long options: ' . var_export($input->getLongOptions(), true) . PHP_EOL;
+echo 'short options: ' . var_export($input->getShortOptions(), true) . PHP_EOL;
+echo 'unhandled arguments: ' . var_export($input->getUnhandled(), true) . PHP_EOL;
+echo PHP_EOL;
+echo 'all arguments: ' . var_export($input->getArguments(), true) . PHP_EOL;
+echo PHP_EOL;
 echo PHP_EOL;
 echo 'Usage: ' . PHP_EOL;
 echo '    ' . basename(__FILE__) . ' --long-option -short-option key=value' . PHP_EOL;
