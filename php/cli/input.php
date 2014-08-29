0c1406cbafee0c4e5a2865b72c55191ee083c6de
stevleibelt <stev@leibelt.de>
2014-08-29 18:09:35 +0200

    foo

--- php/cli/input.php
+++ php/cli/input.php
@@ -1,22 +1,254 @@
 <?php
 /**
  * @author stev leibelt <artodeto@bazzline.net>
- * @since 2014-08-27 
+ * @since 2014-08-20
  */
 
-require_once 'Input.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'string' . DIRECTORY_SEPARATOR . 'String.php';
 
-$filePath = array_shift($argv);
-$input = new Input(new String(), $argv);
+/**
+ * Class Input
+ */
+class Input
+{
+    /**
+     * @var array
+     */
+    private $arguments;
 
-echo 'number of arguments: ' . $input->getNumberOfArguments() . PHP_EOL;
-echo 'parameters: ' . var_export($input->getParameters(), true) . PHP_EOL;
-echo 'long options: ' . var_export($input->getLongOptions(), true) . PHP_EOL;
-echo 'short options: ' . var_export($input->getShortOptions(), true) . PHP_EOL;
-echo 'unhandled arguments: ' . var_export($input->getUnhandled(), true) . PHP_EOL;
-echo PHP_EOL;
-echo 'all arguments: ' . var_export($input->getArguments(), true) . PHP_EOL;
-echo PHP_EOL;
-echo PHP_EOL;
-echo 'Usage: ' . PHP_EOL;
-echo '    ' . basename(__FILE__) . ' --long-option -short-option key=value' . PHP_EOL;
+    /**
+     * @var array
+     */
+    private $longOptions;
+
+    /**
+     * @var int
+     */
+    private $numberOfArguments;
+
+    /**
+     * @var array
+     */
+    private $parameters;
+
+    /**
+     * @var array
+     */
+    private $shortOptions;
+
+    /**
+     * @var String
+     */
+    private $string;
+
+    /**
+     * @var array
+     */
+    private $unhandled;
+
+    /**
+     * @param String $string
+     * @param array $arguments
+     */
+    public function __construct(String $string, array $arguments = array())
+    {
+        $this->longOptions = array();
+        $this->parameters = array();
+        $this->shortOptions = array();
+        $this->string = $string;
+        $this->unhandeld = array();
+
+        $this->setArguments($arguments);
+    }
+
+    /**
+     * @return array
+     */
+    public function getArguments()
+    {
+        return $this->arguments;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumberOfArguments()
+    {
+        return $this->numberOfArguments;
+    }
+
+    /**
+     * @param string $name
+     * @param null $default
+     * @return null
+     */
+    public function getParameterValue($name, $default = null)
+    {
+        if ($this->hasParameter($name)) {
+            $value = $this->parameters[$name];
+        } else {
+            $value = $default;
+        }
+
+        return $value;
+    }
+
+    /**
+     * @return array
+     */
+    public function getUnhandled()
+    {
+        return $this->unhandeld;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function hasParameter($name)
+    {
+        return (isset($this->parameters[$name]));
+    }
+
+    /**
+     * @param string $name
+     */
+    public function removeParameters($name)
+    {
+        if ($this->hasParameter($name)) {
+            unset($this->parameters[$name]);
+            $this->update();
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getLongOptions()
+    {
+        return $this->longOptions;
+    }
+
+    /**
+     * @return array
+     */
+    public function getShortOptions()
+    {
+        return $this->shortOptions;
+    }
+
+    /**
+     * @param $name
+     * @return bool
+     */
+    public function hasLongOption($name)
+    {
+        return (isset($this->longOptions[$name]));
+    }
+
+    /**
+     * @param $longOrShortName
+     * @return bool
+     */
+    public function hasOption($longOrShortName)
+    {
+        $hasOption = $this->hasLongOption($longOrShortName);
+
+        if (!$hasOption) {
+            $hasOption = $this->hasShortOption($longOrShortName);
+        }
+
+        return $hasOption;
+    }
+
+    /**
+     * @param $name
+     * @return bool
+     */
+    public function hasShortOption($name)
+    {
+        return (isset($this->shortOptions[$name]));
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasUnhandled()
+    {
+        return (!empty($this->unhandeld));
+    }
+
+    /**
+     * @param string $name
+     */
+    public function removeLongOption($name)
+    {
+        if ($this->hasLongOption($name)) {
+            unset($this->longOptions[$name]);
+            $this->update();
+        }
+    }
+
+    /**
+     * @param string $longOrShortName
+     */
+    public function removeOption($longOrShortName)
+    {
+        if ($this->hasLongOption($longOrShortName)) {
+            unset($this->longOptions[$longOrShortName]);
+            $this->update();
+        } else if ($this->hasShortOption($longOrShortName)) {
+            unset($this->shortOptions[$longOrShortName]);
+            $this->update();
+        }
+    }
+
+    /**
+     * @param string $name
+     */
+    public function removeShortOption($name)
+    {
+        if ($this->hasShortOption($name)) {
+            unset($this->shortOptions[$name]);
+            $this->update();
+        }
+    }
+
+    /**
+     * @param array $arguments
+     */
+    public function setArguments(array $arguments)
+    {
+        foreach ($arguments as $argument) {
+            if ($this->string->startsWith($argument, '--')) {
+                $this->longOptions[$this->string->cut($argument, 2)] = true;
+            } else if ($this->string->startsWith($argument, '-')) {
+                $this->shortOptions[$this->string->cut($argument, 1)] = true;
+            } else {
+                $parts = explode('=', $argument);
+                if (count($parts) > 1) {
+                    $name = array_shift($parts);
+                    $this->parameters[$name] = implode('=', $parts);
+                } else {
+                    $this->unhandeld[] = $argument;
+                }
+            }
+        }
+
+        $this->update();
+    }
+
+    private function update()
+    {
+        $this->arguments = array_merge($this->parameters, $this->shortOptions, $this->longOptions, $this->unhandeld);
+        $this->numberOfArguments = count($this->arguments);
+    }
+}
