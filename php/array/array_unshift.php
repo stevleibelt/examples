b822be44dc9f08071e7c813c936c2be635359e98
stevleibelt <stev@leibelt.de>
2014-09-09 20:57:47 +0200

    added example for array_unshift

---
+++ php/array/array_unshift.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2014-09-09
+ * @see
+ *  http://de2.php.net/manual/en/function.array-unshift.php
+ */
+
+$array = array('three', 'four', 'five');
+
+echo 'array content: ' . var_export($array, true) . PHP_EOL;
+
+array_unshift($array, 'two');
+array_unshift($array, array('one'));
+
+echo 'array content: ' . var_export($array, true) . PHP_EOL;
