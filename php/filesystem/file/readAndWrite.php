6c7b78e7d968789ee9989c9dcee4cfe60412b34e
stevleibelt <stev@leibelt.de>
2015-03-02 10:59:20 +0100

    renamed and extended with read example

--- php/filesystem/file/readAndWrite.php
+++ php/filesystem/file/readAndWrite.php
@@ -7,9 +7,14 @@
 $content = 'This is a content line';
 $filePath = '/tmp/bazzlineScriptsFOpen_' . getmypid();
 
+# write
+
 $fh = fopen($filePath, 'w');
 
 $numberOfBytesWritten = fwrite($fh, $content);
+if ($numberOfBytesWritten === false) {
+    exit('could not write content "' . $content . '" into file "' . $filePath . '"');
+}
 $filePointerPosition = ftell($fh);
 
 fclose($fh);
@@ -20,5 +25,23 @@ echo 'Written content "' . $content . '"' . PHP_EOL;
 echo 'Number of bytes written "' . $numberOfBytesWritten . '"' . PHP_EOL;
 echo 'Current position of file handler "' . $filePointerPosition . '"' . PHP_EOL;
 echo '----------------' . PHP_EOL;
+
+echo PHP_EOL;
+
+# read
+
+$fh = fopen($filePath, 'r');
+
+echo '----------------' . PHP_EOL;
+echo 'outputting content of file "' . $filePath . '"' . PHP_EOL;
+while (!feof($fh)) {
+    $line = fgets($fh) . PHP_EOL;
+    if (!feof($fh)) {
+        echo $line . PHP_EOL;
+    }
+}
+echo '----------------' . PHP_EOL;
+
+fclose($fh);
 
 unlink($filePath);
