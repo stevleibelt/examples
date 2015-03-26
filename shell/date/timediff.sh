94a4c9d9f7dc8bd895214448eeb19464f7e06e2d
stevleibelt <stev@leibelt.de>
2015-03-26 14:31:13 +0100

    added example for sleep and timediff

---
+++ shell/date/timediff.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+########
+# Example for using date.
+#
+# @author stev leibelt <artodeto@bazzline.net>
+# @since 2015-03-26
+########
+
+if [ "$#" -gt 0 ]; then
+    SLEEP="$1"
+else
+    SLEEP=7
+fi
+
+START_TIMESTAMP=$(date +'%s')
+echo 'sleeping for '$SLEEP' seconds'
+sleep $SLEEP's'
+END_TIMESTAMP=$(date +'%s')
+DIFF=$(($END_TIMESTAMP-$START_TIMESTAMP))
+
+echo 'Runtime: '$DIFF' in seconds'
---
+++ shell/misc/sleep.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+####
+# Example of how to use heredoc
+#
+# @author stev leibelt <artodeto@bazzline.net>
+# @since 2015-03-26
+# @see
+#   http://www.cyberciti.biz/faq/linux-unix-sleep-bash-scripting/
+####
+
+function echoDate ()
+{
+    echo $(date +'%Y-%m-%d %H:%M:%S')
+}
+
+echoDate
+echo 'sleep for 1 second'
+sleep 1s
+#also valid
+#sleep 1
+echoDate
+echo '----'
+
+echoDate
+echo 'sleep for 1 minute'
+sleep 1m
+echoDate
+echo '----'
+
+echoDate
+echo 'sleep for 1 hour'
+sleep 1h
+echoDate
+echo '----'
+
+echoDate
+echo 'sleep for 1 day'
+sleep 1d
+echoDate
+echo '----'
