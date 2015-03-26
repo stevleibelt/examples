6e244284aff1cc04042b6ee8e766cde1902a2761
stevleibelt <stev@leibelt.de>
2015-03-26 14:51:14 +0100

    added timestamp example ad fixed styling

--- shell/date/date.sh
+++ shell/date/date.sh
@@ -2,28 +2,28 @@
 ########
 # Example for using date.
 #
-# @author stev leibelt
-# @2013-02-02
+# @author stev leibelt <artodeto@bazzline.net>
+# @since 2013-02-02
 ########
 
-echo 'date +%D'
+echo "date +'%D'"
 date +'%D'
 echo ''
 
-echo 'date +%T'
+echo "date +'%T'"
 date +'%T'
 echo ''
 
-echo 'date +%H-%M'
+echo "date +'%H-%M'"
 date +'%H+%M'
 echo ''
 
-echo 'date +%y-%m-%d'
+echo "date +'%y-%m-%d'"
 DATE=$(date +'%y-%m-%d')
 echo $DATE
 echo ''
 
-echo 'date +%Y-%m-%d %H:%M:%S'
+echo "date +'%Y-%m-%d %H:%M:%S'"
 DATE=$(date +'%Y-%m-%d %H:%M:%S')
 echo $DATE
 echo ''
---
+++ shell/date/timestamp.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+########
+# Example for using date.
+#
+# @author stev leibelt <artodeto@bazzline.net>
+# @since 2015-03-26
+########
+
+echo 'from timestamp to date'
+TIMESTAMP=424044000
+echo "date -d @'$TIMESTAMP' +'%Y-%m-%d %H:%M:%S'"
+echo $(date -d @$TIMESTAMP +'%Y-%m-%d %H:%M:%S')
+echo ''
+
+echo 'from date to timestamp'
+DATE='06/10/1983'
+echo "date -d '$DATE' +'%s'"
+echo $(date -d $DATE +'%s')
+echo ''
