a868c106b12ebdc8a6cd736ce920c1fb4dffd0aa
stevleibelt <stev@leibelt.de>
2015-06-26 15:27:15 +0200

    added links

--- php/cli/stdin.php
+++ php/cli/stdin.php
@@ -3,6 +3,9 @@
 /**
  * @author stev leibelt <artodeto@bazzline.net>
  * @since 2015-06-25
+ * @see:
+ *  http://php.net/manual/en/features.commandline.io-streams.php
+ *  http://.php.net/manual/en/function.readline.php
  */
 
 $stdin  = fopen('php://stdin', 'r');
