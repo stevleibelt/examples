c9af1d508fd09719d099e442e824b98f8d7f3337
stevleibelt <stev@leibelt.de>
2015-10-07 16:33:29 +0200

    added an other approach without using realpath

--- shell/directory/realpath.sh
+++ shell/directory/realpath.sh
@@ -9,8 +9,17 @@
 ## get path of the current script
 CURRENT_SCRIPT_PATH=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
 ONE_DIRECTORY_ABOVE=$(realpath $CURRENT_SCRIPT_PATH/../)
+WITHOUT_REAL_PATH=$(cd $(dirname "${BASH_SOURCE[0]}"); cd ..; pwd)
 
-echo "current script path: "$CURRENT_SCRIPT_PATH
-echo "parent path: "$ONE_DIRECTORY_ABOVE
+echo "current script path: "
+echo "    "$CURRENT_SCRIPT_PATH
+echo ""
+
+echo "parent path: "
+echo "    "$ONE_DIRECTORY_ABOVE
+echo ""
+
+echo "parent path without real path: "
+echo "    "$WITHOUT_REAL_PATH
 
 exit 0
