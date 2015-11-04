5d9a3e9bd37f4fb3d49decb300d97ba480a1bc66
stevleibelt <stev@leibelt.de>
2015-11-04 17:29:10 +0100

    added strtotime test

--- php/date/dateTime.0000-00-00.php
+++ php/date/dateTime.0000-00-00.php
@@ -6,7 +6,11 @@
 
 date_default_timezone_set('Europe/Berlin');
 
-$dateTime = new DateTime('0000-00-00');
+$date       = '0000-00-00';
+$dateTime   = new DateTime($date);
 
+echo 'date: ' . $date . PHP_EOL;
 echo 'format(\'Y-md H:i:s\'): ' . $dateTime->format('Y-m-d H:i:s') . PHP_EOL;
 echo 'format(\'U\'): ' . $dateTime->format('U') . PHP_EOL;
+echo 'strtotime: ' . strtotime($date) . PHP_EOL;
+echo 'strtotime (date(\'Ymd H:i:s\'): ' . date('Ymd H:i:s', strtotime($date)) . PHP_EOL;
