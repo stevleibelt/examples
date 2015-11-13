c5a3e22ab6f432c9d637e3e081c81fae3150438b
stevleibelt <stev@leibelt.de>
2015-11-13 15:02:38 +0100

    added an example howto list all available file extions from given path

---
+++ shell/file/listAllAvailableFileExtensionsFromPath.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+####
+# @author stev leibelt <artodeto@bazzline.net>
+# @since 2015-11-13
+####
+
+if [[ $# -eq 0 ]]; then
+    PATH_TO_SEARCH_IN="."
+else
+    PATH_TO_SEARCH_IN="$1"
+fi
+
+find $PATH_TO_SEARCH_IN -type f -exec basename \{\} \; | rev | cut -d'.' -f 1 | rev | sort | uniq
