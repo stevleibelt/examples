e819fdb5ab642035ad125c3adc766c9225d1430b
stevleibelt <stev@leibelt.de>
2016-02-02 09:34:56 +0100

    added logger example

---
+++ shell/misc/logger.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+####
+# Example of how to use heredoc
+#
+# @author stev leibelt <artodeto@bazzline.net>
+# @since 2016-02-02
+####
+
+LOCAL_DATE=$(date +'%Y-%m-%d %H:%M:%S')
+LOCAL_SCRIPT_PATH=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
+LOCAL_IP=$(hostname -i)
+
+LOCAL_LOG_MESSAGE="[$LOCAL_DATE]: $HOSTNAME - $LOCAL_IP"
+
+# either output it
+echo $LOCAL_LOG_MESSAGE
+#or put it into a file
+#echo $LOCAL_LOG_MESSAGE >> $LOCAL_SCRIPT_PATH"/logger.log"
