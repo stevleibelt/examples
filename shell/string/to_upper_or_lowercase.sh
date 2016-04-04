c7f0d8f3fbc43b8a8a4efbedba54dbdba065625a
stevleibelt <stev@leibelt.de>
2016-04-04 17:35:12 +0200

    added example to uppercase or lowercase in bash

---
+++ shell/string/to_upper_or_lowercase.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+####
+# @author stev leibelt <artodeto@bazzline.net>
+# @since 2016-04-04
+# @see
+#   https://bash.cyberciti.biz/guide/Howto:_convert_string_to_all_uppercase
+####
+
+echo "outputting your arguments"
+    echo ""
+
+for LOCAL_STRING in "$@"; do
+    echo "your argument:  $LOCAL_STRING"
+    echo "  to uppercase: "$(echo $LOCAL_STRING | tr [a-z] [A-Z])
+    echo "  to lowercase: "$(echo $LOCAL_STRING | tr [A-Z] [a-z])
+done
