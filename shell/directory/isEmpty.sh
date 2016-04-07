ec6cd6235ef3c7d7a1b515c3a0f4fbbdcdeca761
stevleibelt <stev@leibelt.de>
2016-04-07 14:52:13 +0200

    added example if it isi empty

---
+++ shell/directory/isEmpty.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+########
+# @author stev leibelt <artodeto@bazzline.net>
+# @since 2016-04-07
+# @see http://www.cyberciti.biz/faq/linux-unix-shell-check-if-directory-empty/
+########
+
+if [[ $# -eq 0 ]]
+then
+    LOCAL_PATH_TO_THE_DIRECTORY="."
+else
+    LOCAL_PATH_TO_THE_DIRECTORY="$1"
+fi
+
+echo "using path $LOCAL_PATH_TO_THE_DIRECTORY"
+
+if [[ "$(ls -A $LOCAL_PATH_TO_THE_DIRECTORY)" ]]
+then
+    echo "directory is not empty"
+else
+    echo "directory is empty"
+fi
