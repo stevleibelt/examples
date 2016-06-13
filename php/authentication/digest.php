64c2d35e1166fb1a21a21722e111f4aa50595a25
stevleibelt <stev@leibelt.de>
2016-06-13 21:34:49 +0200

    added digest

---
+++ php/authentication/digest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @see 
+ *  https://en.wikipedia.org/wiki/Digest_access_authentication
+ *  http://www.unixpapa.com/auth/basic.html
+ *  https://www.sitepoint.com/understanding-http-digest-access-authentication/
+ *  https://evertpot.com/223/
+ *  https://github.com/phpmasterdotcom/UnderstandingHTTPDigest
+ *  http://php.net/manual/en/features.http-auth.php
+ * @author stev leibelt <artodeto@bazzline.net>
+ * @since 2016-06-13
+ */
+
+echo 'read the links. there is no easy encoding or decoding since the server has to create a randomized nounce and since the uri is also important' . PHP_EOL;
