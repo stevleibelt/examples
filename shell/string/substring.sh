7be293967346fb1705a00c06e448a86d83096c88
stevleibelt <stev@leibelt.de>
2016-08-27 22:39:32 +0200

    added

--- shell/string/substring.sh
+++ shell/string/substring.sh
@@ -9,12 +9,14 @@
 #    * https://www.linuxquestions.org/questions/linux-newbie-8/simple-bash-script-help-grabbing-part-of-a-string-316917/
 ####
 
-STRING='FooBar-1.2.3-baz.tar.gz';
+STRING="FooBar-1.2.3-baz.tar.gz";
 
 #substringing with positions
 WITHOUT_FOO=${STRING:3};
 ONLY_NUMBERS=${STRING:7:5};
+LAST_CHARACTER=${STRING: -1};
 
-echo 'string is: '$STRING;
-echo 'without Foo: '$WITHOUT_FOO;
-echo 'only numbers: '$ONLY_NUMBERS;
+echo "string is: ${STRING}"
+echo "without Foo: ${WITHOUT_FOO}"
+echo "only numbers: ${ONLY_NUMBERS}"
+echo "last charachter: ${LAST_CHARACTER}"
