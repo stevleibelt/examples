73ba0d548779fea5cf7fb6ab2c7abfaabc7a9beb
stevleibelt <stev@leibelt.de>
2016-08-30 12:41:02 +0200

    updated

--- shell/input/getopts.sh
+++ shell/input/getopts.sh
@@ -4,4 +4,50 @@
 # @since 2014-03-25
 ####
 
-getopts ABC:D VAR
+CONTENT_OF_ARGUMENT_D=""
+FLAG_A_IS_ENABLED=0
+FLAG_B_IS_ENABLED=0
+FLAG_C_IS_ENABLED=0
+SHOW_USAGE=1
+
+#a, b and c are simple flags
+#d has an argument
+#if you put this into a function, you have to provide the function the arguments like <function name> $@
+while getopts ":abcd:" CURRENT_OPTION;
+do
+    case ${CURRENT_OPTION} in
+        a )
+            FLAG_A_IS_ENABLED=1
+            ;;
+        b )
+            FLAG_B_IS_ENABLED=1
+            ;;
+        c )
+            FLAG_C_IS_ENABLED=1
+            ;;
+    esac
+done
+
+if [[ ${FLAG_A_IS_ENABLED} -eq 1 ]];
+then
+    echo "flag a was provided"
+    SHOW_USAGE=0
+fi
+
+if [[ ${FLAG_B_IS_ENABLED} -eq 1 ]];
+then
+    echo "flag b was provided"
+    SHOW_USAGE=0
+fi
+
+if [[ ${FLAG_C_IS_ENABLED} -eq 1 ]];
+then
+    echo "flag c was provided"
+    SHOW_USAGE=0
+fi
+
+if [[ ${SHOW_USAGE} -eq 1 ]];
+then
+    echo ":: Usage"
+    echo "   <command> [-a] [-b] [-c]"
+fi
--- shell/loop/while.sh
+++ shell/loop/while.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+####
+# @author stev leibelt <artodeto@bazzline.net>
+# @since 2015-09-03
+####
+
 ITERATOR=1
 LIMIT=10
+
 while [ $ITERATOR -le $LIMIT ]
 do
     echo $ITERATOR' run'
