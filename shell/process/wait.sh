cd022882cfb3625410952b33c85b475c60833ce1
stevleibelt <stev@leibelt.de>
2016-09-02 14:35:38 +0200

    added prevent screen timeout foo

--- shell/process/wait.sh
+++ shell/process/wait.sh
@@ -13,22 +13,49 @@ set -o monitor
 
 SCRIPT_PATH=$(cd $(dirname "$0"); pwd)
 
-bash $SCRIPT_PATH/sleepy_process.sh 2 &
+bash ${SCRIPT_PATH}/sleepy_process.sh 2 &
 PID_ONE=$!
 
-bash $SCRIPT_PATH/sleepy_process.sh 3 &
+bash ${SCRIPT_PATH}/sleepy_process.sh 3 &
 PID_TWO=$!
 
-echo "."
-sleep 0.5
-echo "."
+bash ${SCRIPT_PATH}/sleepy_process.sh 5 &
+PID_THREE=$!
 
-wait $PID_ONE
+wait ${PID_ONE}
 echo 'PID ONE has ended'
 
-echo "."
-sleep 0.5
-echo "."
+#begin of keeping the session alive
+echo ""
+ITERATOR=0;
+
+#store current curser position
+printf "\033[s"
+
+#run until the pid is not detected anymore
+while ps -p ${PID_THREE} | grep -q ${PID_THREE};
+do
+    if [[ ${ITERATOR} -eq 0 ]];
+    then 
+        ((++ITERATOR))
+        printf "\033[u[-]"
+    elif [[ ${ITERATOR} -eq 1 ]];
+    then
+        ((++ITERATOR))
+        printf "\033[u[\]"
+    elif [[ $ITERATOR -eq 2 ]];
+    then
+        ((++ITERATOR))
+        printf "\033[u[|]"
+    else
+        ITERATOR=0
+        printf "\033[u[/]"
+    fi
+
+    sleep 0.5
+done;
+printf "\033[K"
+#end of keeping the session alive
 
 wait
 echo 'all background processes are done'
