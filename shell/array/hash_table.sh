caa4e3b7f665e8558b46f283eaad9bb4ebc2a31f
stevleibelt <stev@leibelt.de>
2016-09-04 11:29:30 +0200

    added example how to sort an array

--- shell/array/hash_table.sh
+++ shell/array/hash_table.sh
@@ -10,25 +10,26 @@
 #it is important to have the space betwee "(" and "["
 declare -A ARRAY_ENTRIES=( ["foo"]="bar" ["bar"]="foo" )
 
-echo "Array has "${#ARRAY_ENTRIES[@]}" elements."
-echo "First entry has "${#ARRAY_ENTRIES}" characters."
-echo "Adding entry \"baz => foz\" to the array."
+echo ":: Array has "${#ARRAY_ENTRIES[@]}" elements."
+echo ":: First entry has "${#ARRAY_ENTRIES}" characters."
+echo ":: Adding entry \"baz => foz\" to the array."
+echo ""
 #add entry to the array
 ARRAY_ENTRIES+=(["baz"]="foz")
 
 #! expands the key
 for ARRAY_KEY in ${!ARRAY_ENTRIES[@]}; do
-    #echo "$ARRAY_KEY - ${ARRAY_ENTRIES["$ARRAY_KEY"]}";
-    echo "$ARRAY_KEY => ${ARRAY_ENTRIES["$ARRAY_KEY"]}";
+    echo "   ${ARRAY_KEY} => ${ARRAY_ENTRIES["${ARRAY_KEY}"]}";
 done;
 
-echo ''
-echo "Calling two elements, starting from second position: "${ARRAY_ENTRIES[@]:1:2}
+echo ""
+echo ":: Calling two elements, starting from second position: "${ARRAY_ENTRIES[@]:1:2}
+echo ""
 
 #inarray
 if [[ ${ARRAY_ENTRIES[*]} == bar ]];
 then
-  echo "There is a bar in the foo!"
+  echo ":: There is a bar in the foo!"
 fi
 
 #array key or index exist
@@ -36,7 +37,7 @@ fi
 #if [[ ${ARRAY_ENTRIES["bar"]}+exists ]];
 if [[ ! -z ${ARRAY_ENTRIES["bar"]} ]];
 then
-    echo "key \"bar\" exists"
+    echo "   Key \"bar\" exists."
 fi
 
 #remove first internal variable
