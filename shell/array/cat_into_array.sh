fc5657faa2be154cd683312927c9cab31192d8fd
stevleibelt <stev@leibelt.de>
2016-10-16 18:20:06 +0200

    added cat_into_array

---
+++ shell/array/cat_into_array.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+########
+# @author stevleibelt
+# @since 2016-10-16
+########
+
+declare -a SCRIPT_LINES_AS_ARRAY=( )
+
+SCRIPT_LINES_AS_ARRAY=( $(cat $0) )
+
+#echo ":: Outputting content of this script line by line"
+#echo ""
+
+echo ${SCRIPT_LINES_AS_ARRAY}
+
+for INDEX_KEY in "${!SCRIPT_LINES_AS_ARRAY[@]}";
+do
+    echo "line number ${INDEX_KEY}: ${SCRIPT_LINES_AS_ARRAY[${INDEX_KEY}]}"
+done;
