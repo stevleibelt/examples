4484d3d0af56fa43dbb5ecf5552df5e5693ab9bc
stevleibelt <stev@leibelt.de>
2016-12-02 10:02:44 +0100

    style

--- php/class/get_class.php
+++ php/class/get_class.php
@@ -14,4 +14,9 @@ class MyClass
 $myClass = new MyClass();
 
 //echo $myClass must be an object
-echo 'get_class($myClass): ' . get_class($myClass) . PHP_EOL;
+echo ':: Outputting ful qualified class name.' . PHP_EOL;
+echo get_class($myClass) . PHP_EOL;
+echo PHP_EOL;
+
+echo ':: Outputting the class name.' . PHP_EOL;
+echo basename(str_replace('\\', '/', get_class($myClass))) . PHP_EOL;
