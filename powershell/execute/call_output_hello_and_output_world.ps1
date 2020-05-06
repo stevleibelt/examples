6b7fdab7c4c3f2f6f6c386efd971076b23b353fc
stevleibelt <stev@leibelt.de>
2020-05-06 09:50:43 +0200

    added powershell example how to start one ps1 from inside another

---
+++ powershell/execute/call_output_hello_and_output_world.ps1
@@ -0,0 +1,8 @@
+#!/usr/bin/env pwsh
+####
+# @since 2020-05-06
+# @author stev leibelt <artodeto@bazzline.net>
+####
+
+Invoke-Expression -Command .\output_hello.ps1
+Invoke-Expression -Command .\output_world.ps1
---
+++ powershell/execute/output_world.ps1
@@ -0,0 +1,7 @@
+#!/usr/bin/env pwsh
+####
+# @since 2020-05-06
+# @author stev leibelt <artodeto@bazzline.net>
+####
+
+Write-Host "World"
