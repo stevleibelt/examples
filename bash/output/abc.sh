28904d583a43df597145a7b46d5ae68eae50ceca
stevleibelt <stev@leibelt.de>
2021-03-14 21:23:15 +0100

    added abc

---
+++ bash/output/abc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+####
+# loop through the alphabet
+####
+# @since 2021-03-14
+# @author stev leibelt <artodeto@bazzline.net>
+####
+
+for CHARACTER in {a..z};
+do
+    echo ${CHARACTER}
+done
