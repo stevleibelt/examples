a7779bb7b7def9ae313e430e22f4e5b8ff6f9277
stevleibelt <stev@leibelt.de>
2021-05-28 13:04:46 +0200

    updated

--- bash/input/getopt.sh
+++ bash/input/getopt.sh
@@ -2,6 +2,7 @@
 ####
 # @see
 #   https://stackoverflow.com/questions/402377/using-getopts-in-bash-shell-script-to-get-long-and-short-command-line-options
+#   https://opensource.com/article/21/5/launch-flatpaks-linux-terminal
 # @author stev leibelt <artodeto@bazzline.net>
 # @since 2018-08-13
 ####
@@ -20,15 +21,19 @@ do
     case "${1}" in
         -a )
             FLAG_A_IS_ENABLED=1
-            shift
+            shift 1
             ;;
         -b )
             FLAG_B_IS_ENABLED=1
-            shift
+            shift 1
             ;;
         -c )
             FLAG_C_IS_ENABLED=1
-            shift
+            shift 1
+            ;;
+        -d )
+            CONTENT_OF_ARGUMENT_D="${2}"
+            shift 2
             ;;
         *)
             break
@@ -38,24 +43,31 @@ done
 
 if [[ ${FLAG_A_IS_ENABLED} -eq 1 ]];
 then
-    echo "flag a was provided"
+    echo ":: Flag a was provided"
     SHOW_USAGE=0
 fi
 
 if [[ ${FLAG_B_IS_ENABLED} -eq 1 ]];
 then
-    echo "flag b was provided"
+    echo ":: Flag b was provided"
     SHOW_USAGE=0
 fi
 
 if [[ ${FLAG_C_IS_ENABLED} -eq 1 ]];
 then
-    echo "flag c was provided"
+    echo ":: Flag c was provided"
+    SHOW_USAGE=0
+fi
+
+if [[ ${CONTENT_OF_ARGUMENT_D} != "" ]];
+then
+    echo ":: Content after -d."
+    echo "   >>${CONTENT_OF_ARGUMENT_D}<<"
     SHOW_USAGE=0
 fi
 
 if [[ ${SHOW_USAGE} -eq 1 ]];
 then
     echo ":: Usage"
-    echo "   <command> [-a] [-b] [-c]"
+    echo "   <command> [-a] [-b] [-c] [-d \"<content>\"]"
 fi
