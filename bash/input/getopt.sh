9994154e15bd7f5a7b951ef8f7d8cb5843b4535e
stevleibelt <stev@leibelt.de>
2021-08-24 15:16:35 +0200

    added support for long arguments

--- bash/input/getopt.sh
+++ bash/input/getopt.sh
@@ -3,6 +3,7 @@
 # @see
 #   https://stackoverflow.com/questions/402377/using-getopts-in-bash-shell-script-to-get-long-and-short-command-line-options
 #   https://opensource.com/article/21/5/launch-flatpaks-linux-terminal
+#   https://opensource.com/article/21/8/option-parsing-bash
 # @author stev leibelt <artodeto@bazzline.net>
 # @since 2018-08-13
 ####
@@ -19,7 +20,7 @@ SHOW_USAGE=1
 while true;
 do
     case "${1}" in
-        -a )
+        -a | "--alpha" )
             FLAG_A_IS_ENABLED=1
             shift 1
             ;;
@@ -31,7 +32,7 @@ do
             FLAG_C_IS_ENABLED=1
             shift 1
             ;;
-        -d )
+        -d | "--delta")
             CONTENT_OF_ARGUMENT_D="${2}"
             shift 2
             ;;
@@ -43,31 +44,42 @@ done
 
 if [[ ${FLAG_A_IS_ENABLED} -eq 1 ]];
 then
-    echo ":: Flag a was provided"
+    echo ":: Flag -a or --alpha was provided"
     SHOW_USAGE=0
 fi
 
 if [[ ${FLAG_B_IS_ENABLED} -eq 1 ]];
 then
-    echo ":: Flag b was provided"
+    echo ":: Flag -b was provided"
     SHOW_USAGE=0
 fi
 
 if [[ ${FLAG_C_IS_ENABLED} -eq 1 ]];
 then
-    echo ":: Flag c was provided"
+    echo ":: Flag -c was provided"
     SHOW_USAGE=0
 fi
 
 if [[ ${CONTENT_OF_ARGUMENT_D} != "" ]];
 then
-    echo ":: Content after -d."
+    echo ":: Content after -d or --delta ."
     echo "   >>${CONTENT_OF_ARGUMENT_D}<<"
+    SHOW_USAGE=0
+fi
+
+if [[ $# -gt 0 ]];
+then
+    echo ":: Dumping ${#} arguments."
+
+    for CURRENT_ARGUMENT in ${@}; do
+        echo "   >>${CURRENT_ARGUMENT}<<"
+    done
+
     SHOW_USAGE=0
 fi
 
 if [[ ${SHOW_USAGE} -eq 1 ]];
 then
     echo ":: Usage"
-    echo "   <command> [-a] [-b] [-c] [-d \"<content>\"]"
+    echo "   <command> [-a|--alpha] [-b] [-c] [-d \"<content>\"|--delta=\"<content>\"]"
 fi
