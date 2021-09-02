0c8ba92e54d6fc3e161e82f5262f319f54e43fb0
stevleibelt <stev@leibelt.de>
2021-09-02 10:27:53 +0200

    extended example

--- bash/function/return_code.sh
+++ bash/function/return_code.sh
@@ -6,11 +6,38 @@
 # @since 2015-09-03
 ####
 
-ls -halt .
-echo 'previous command returned return code: "'$?'"'
+function _reacting_on_return_code ()
+{
+    _generate_return_code 0
+    echo "   Previous method return code >>${?}<<."
 
+    _generate_return_code 76
+    echo "   Previous method return code >>${?}<<."
+
+    echo ":: Demonstrating a wrong usage of the return code."
+    _generate_return_code "foo"
+    echo "   Previous method return code >>${?}<<."
+}
+
+####
+# <param> <return_code>
+####
+function _generate_return_code ()
+{
+    local RETURN_CODE="${1:-0}"
+
+    return ${RETURN_CODE}
+}
+
+echo ":: Executing >>ls -halt .<<."
+ls -halt .
+echo "   Previous command returned return code >>${?}<<."
+
+echo ":: Executing >>ls -halt /root<<."
 ls -halt /root
 echo -e "\e[41m" #set background to bold red: https://wiki.archlinux.org/index.php/Color_Bash_Prompt
-echo ''
-echo 'previous command returned return code: "'$?'"'
+echo ""
+echo "   Previous command returned return code >>${?}<<."
 echo -e "\e[0m" #reset font
+
+_reacting_on_return_code
