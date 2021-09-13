33e1ec8326e7ee2701efe5852f943fb2a714d4a5
stevleibelt <stev@leibelt.de>
2021-09-13 14:17:15 +0200

    added check for sha512

--- powershell/filesystem/check_checksum.ps1
+++ powershell/filesystem/check_checksum.ps1
@@ -8,12 +8,13 @@
 
 $pathToBinaryFile = $($PSScriptRoot + [IO.Path]::DirectorySeparatorChar + "data" + [IO.Path]::DirectorySeparatorChar + "foo")
 $pathToSha1File = $($pathToBinaryFile + ".sha1")
+$pathToOpenHashTabSha512File = $($pathToBinaryFile + ".sha512")
 
 If (test-path $pathToSha1File) {
     $calculatedFileHash = Get-FileHash -LiteralPath $pathToBinaryFile -Algorithm SHA1
     $sha1FileContent = Get-Content $pathToSha1File
 
-    Write-Host ":: Checking file integrity."
+    Write-Host ":: Checking file integrity with sha1."
     #we are expecting a sha1 file with one line of content.
     #   this one line should look like:<file name>\t<sha1 sum>
     #we are exploding the expected content by " "
@@ -38,4 +39,33 @@ If (test-path $pathToSha1File) {
     }
 } Else {
     Write-Host $(":: File path invalid >>" + $pathToSha1File + "<<.")
+}
+
+If (test-path $pathToOpenHashTabSha512File) {
+    $calculatedFileHash = Get-FileHash -LiteralPath $pathToBinaryFile -Algorithm SHA512
+    #remove each line starting with >>#<<
+    $hashFileContent = Get-Content $pathToOpenHashTabSha512File | Select-String -pattern "^#" -NotMatch
+
+    Write-Host ":: Checking file integrity with sha512."
+    #we are expeting a sha512 file with multiple lines of comment.
+    #   the first and only line with no comment is the line with the sha512sum and it looks like:<sha512 sum>\t*<file name>
+    #we are exploding the expected content by " "
+    #    first array entry is the <sha512sum>
+    #    ...
+    $arrayOfHashFileContent = $hashFileContent.ToString().Split(" ")
+    $expectedFileHash = $arrayOfHashFileContent[0]
+
+    If ($expectedFileHash.Length -eq 128) {
+        If ($expectedFileHash -ne $calculatedFileHash.HASH) {
+            Write-Host $("   Binary file integrity check failed. Expected checksum >>" + $expectedFileHash + "<<, current checksum >>" + $calculatedFileHash.HASH + "<<.")
+        } else {
+            Write-Host "   All is fine. Checksums are equal."
+        }
+    } Else {
+        Write-Host ":: Unexpected file content, neither first or third part of the sha512 file contains a sha512 checksum string."
+        Exit(1)
+    }
+
+} Else {
+    Write-Host $(":: File path invalid >>" + $pathToOpenHashTabSha512File + "<<.")
 }
