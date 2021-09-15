a01e02886274ab2a70aeb069653108581476996a
stevleibelt <stev@leibelt.de>
2021-09-15 09:47:11 +0200

    added example for user input

---
+++ powershell/input/user-input.ps1
@@ -0,0 +1,22 @@
+#!/usr/bin/env pwsh
+####
+# Example how you can ask for user input
+####
+# @since 2021-09-15
+# @author stev leibelt <artodeto@bazzline.net>
+####
+
+$YesOrNo = Read-Host -Prompt ':: Yes or no? (y/N) '
+$UserName = Read-Host -Prompt ':: Input your user name.'
+
+If ($UserName.Length -eq 0) {
+    $UserName = 'Neo'
+}
+
+Write-Host $($UserName + " ...")
+
+If ($YesOrNo.StartsWith('y')) {
+    Write-Host "   Computer says yes."
+} Else {
+    Write-Host "   Computer says no."
+}
