17b147ebca7eded1d4f33431b1d054154764c5c8
stevleibelt <stev@leibelt.de>
2021-10-18 13:48:53 +0200

    added example how to sort a hash table output plus how to change headlines

--- powershell/output/format.ps1
+++ powershell/output/format.ps1
@@ -20,3 +20,12 @@ $ItemNames = @(
 "Listing first item: >>{0}<<." -f $ItemNames
 
 "{1} of {0}." -f $ItemNames[3], $ItemNames[2]
+
+#Output hash table sorted and rename headline
+
+$HashTable = @{
+    "Name Zzz" = "Value One"
+    "Name Aaa" = "Value Two"
+}
+
+$HashTable.GetEnumerator() | Sort Name | Format-Table @{Label='My alternative for name';Expression={$_.Name}},@{Label='My alternative for value';Expression={$_.Value}} -AutoSize
