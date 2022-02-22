9c7d9f2321bbd6446b6848d2bd31e283eabe7d1d
stevleibelt <stev@leibelt.de>
2022-02-22 21:07:10 +0100

    added

---
+++ bat/start_programm.bat
@@ -0,0 +1,16 @@
+:: ================
+:: Start a programm
+:: 
+:: ================
+:: @see:
+::   https://www.shellhacks.com/start-maximized-minimized-program-cmd-batch/
+:: @since: 2022-02-22
+:: @author: stev leibelt <artodeto@bazzline.net>
+:: ================
+
+@ECHO OFF
+:: start [/MIN | /MAX] "" "c:\path\to\the.exe"
+:: /MIN starts the programm minimized
+:: /MAX starts the programm maximized
+
+exit
