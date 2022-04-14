6a384fa885245be193446f76e7160d62efc5ec78
Stev Leibelt <artodeto@bazzline.net>
2022-04-14 15:45:29 +0200

    added

--- bash/array/hash_table.sh
+++ bash/array/hash_table.sh
@@ -4,6 +4,33 @@
 # @since 2015-11-18
 ########
 
+#works every where
+#super smart way
+#@see: https://stackoverflow.com/a/32591963 20220414T15:42:32
+
+MY_MULTI_VALUE_ARRAY=(
+    '192.168.1.4/24|tcp|22'
+    '192.168.1.4/24|tcp|53'
+    '192.168.1.4/24|tcp|80'
+    '192.168.1.4/24|tcp|139'
+    '192.168.1.4/24|tcp|443'
+    '192.168.1.4/24|tcp|445'
+    '192.168.1.4/24|tcp|631'
+    '192.168.1.4/24|tcp|5901'
+    '192.168.1.4/24|tcp|6566'
+)
+
+for CURRENT_ROW in "${MY_MULTI_VALUE_ARRAY[@]}"
+do
+    IFS=$'|' read -r RANGE PROTO PORT <<< "${CURRENT_ROW}"
+
+    echo ":: CURRENT ROW >>${CURRENT_ROW}<< as columns:"
+    echo "   RANGE >>${RANGE}<<"
+    echo "   PROTO >>${PROTO}<<"
+    echo "   PORT >>${PORT}<<"
+    echo ""
+done
+
 #only works in bash >= 4
 #array
 #http://stackoverflow.com/questions/1494178/how-to-define-hash-tables-in-bash
