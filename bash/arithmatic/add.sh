17518593c3a23c53fc282211656747de0cad4a9a
stevleibelt <stev@leibelt.de>
2022-06-08 21:50:26 +0200

    updated

--- bash/arithmatic/add.sh
+++ bash/arithmatic/add.sh
@@ -5,5 +5,16 @@
 ####
 
 RESULT=`expr 1 + 1`
+VAR=3
+
+VAR=$((VAR+1))
+((VAR=VAR+1))
+((VAR+=1))
+((VAR++))
+
+let "VAR=VAR+1"
+let "VAR+=1"
+let "VAR++"
 
 echo '1 + 1 is: '$RESULT
+echo "\$VAR >>${VAR}<<."
--- bash/misc/sleep.sh
+++ bash/misc/sleep.sh
@@ -13,28 +13,40 @@ function echoDate ()
     echo $(date +'%Y-%m-%d %H:%M:%S')
 }
 
-echoDate
-echo 'sleep for 1 second'
-sleep 1s
-#also valid
-#sleep 1
-echoDate
-echo '----'
+function _main ()
+{
 
-echoDate
-echo 'sleep for 1 minute'
-sleep 1m
-echoDate
-echo '----'
+    echoDate
+    echo ":: Sleep for 1 second"
+    sleep 1s
+    #also valid
+    #sleep 1
+    echoDate
+    echo ""
 
-echoDate
-echo 'sleep for 1 hour'
-sleep 1h
-echoDate
-echo '----'
+    echoDate
+    echo ":: Sleep for 5 second"
+    sleep 5s
+    echoDate
+    echo ""
 
-echoDate
-echo 'sleep for 1 day'
-sleep 1d
-echoDate
-echo '----'
+    echoDate
+    echo ":: Sleep for 1 minute"
+    sleep 1m
+    echoDate
+    echo ""
+
+    echoDate
+    echo ":: Sleep for 1 hour"
+    sleep 1h
+    echoDate
+    echo ""
+
+    echoDate
+    echo ":: Sleep for 1 day"
+    sleep 1d
+    echoDate
+    echo ""
+}
+
+_main
