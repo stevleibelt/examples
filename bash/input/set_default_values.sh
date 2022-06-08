d1d9742a07d465e5d2ae69addfd6dbac95688328
stevleibelt <stev@leibelt.de>
2022-06-08 21:37:09 +0200

    updated

--- bash/input/set_default_values.sh
+++ bash/input/set_default_values.sh
@@ -6,11 +6,13 @@
 
 function _example()
 {
-    VAR=${1:-'foo'}
-    FOO=${2:-''}
+    local VAR=${1:-'foo'}
+    local FOO=${2:-''}
+    local BAR=${3:-3}
 
-    echo "\$VAR: ${VAR}"
-    echo "\$FOO: ${FOO}"
+    echo "\$VAR >>${VAR}<<."
+    echo "\$FOO >>${FOO}<<."
+    echo "\$BAR >>${BAR}<<."
 }
 
 _example ${@}
