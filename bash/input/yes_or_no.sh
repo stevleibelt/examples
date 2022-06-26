af3f733d812e5de7055eee3fa8a4aebc0a9da794
stev leibelt <artodeto@bazzline.net>
2022-06-26 17:44:19 +0200

    added another yes or no example

--- bash/input/yes_or_no.sh
+++ bash/input/yes_or_no.sh
@@ -8,15 +8,32 @@
 # @since 2016-07-01
 ####
 
-while true; do
-    read -p "is everything ok? [y/n]" yn
-    case $yn in
-        [Yy]* ) 
-            echo "go on an hug someone who looks sad";
-            break;;
-        [Nn]* )
-            echo "go on an get a hug from someone";
-            break;;
-        * ) echo "Please answer y or n.";;
-    esac
-done
+function _ask_with_if ()
+{
+    if echo "${1:-n}" | grep -iq '^y$';
+    then
+        echo "   Yes"
+    else
+        echo "   No"
+    fi
+}
+
+function _ask_with_case ()
+{
+    while true; do
+        case ${1:-n} in
+            [Yy]* ) 
+                echo "   Go on and hug someone who looks sad"
+                break;;
+            [Nn]* )
+                echo "   Go on and get a hug from someone"
+                break;;
+            * )
+                echo "   Please input y or n."
+                break;;
+        esac
+    done
+}
+
+_ask_with_if ${1}
+_ask_with_case ${1}
