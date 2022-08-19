881782a3619520798ae83d4555652f2c921d308b
Stev Leibelt <stev.leibelt@hrz.tu-freiberg.de>
2022-08-19 14:04:15 +0200

    styled

--- bash/variable/checkIfVariableExists.sh
+++ bash/variable/checkIfVariableExists.sh
@@ -6,20 +6,26 @@
 #   https://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
 ########
 
-FOO='foo'
+function _main ()
+{
+    local FOO
+    FOO="foo"
 
-if [[ -z ${BAR+x} ]];
-then
-    echo ":: Variable BAR does not exist."
-else
-    echo ":: Variable BAR does exist."
-    echo "   Value is >>${BAR}"
-fi
+    if [[ -z ${BAR+x} ]];
+    then
+        echo ":: Variable BAR does not exist."
+    else
+        echo ":: Variable BAR does exist."
+        echo "   Value is >>${BAR}"
+    fi
 
-if [[ -z ${FOO+x} ]];
-then
-    echo ":: Variable FOO does not exist."
-else
-    echo ":: Variable FOO does exist."
-    echo "   Value is >>${FOO}"
-fi
+    if [[ -z ${FOO+x} ]];
+    then
+        echo ":: Variable FOO does not exist."
+    else
+        echo ":: Variable FOO does exist."
+        echo "   Value is >>${FOO}<<"
+    fi
+}
+
+_main
