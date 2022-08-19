04beeb78bba12bbd0dd727a1833b5ad06cb8478d
Stev Leibelt <stev.leibelt@hrz.tu-freiberg.de>
2022-08-19 11:25:47 +0200

    extended example

--- bash/filesystem/file/step_is_done.sh
+++ bash/filesystem/file/step_is_done.sh
@@ -23,24 +23,30 @@ function execute_current_step ()
 
 function _main
 {
-    local log_file
     local current_step
+    local log_file
+    local step_iterator
 
     echo "usage:: ${0} [<int: already excuted step {0,1,2,3}] [<string: logfile]"
+    echo ""
 
     current_step="${1:-0}"
     log_file="${2:-finished_step.log}"
+    step_iterator=1
 
     if [[ ! -f ${log_file} ]];
     then
         store_finished_step $current_step $log_file
     fi
 
-    execute_current_step 1 ${log_file}
+    execute_current_step ${step_iterator} ${log_file}
+    step_iterator=$((step_iterator+1))
 
-    execute_current_step 2 ${log_file}
+    execute_current_step ${step_iterator} ${log_file}
+    step_iterator=$((step_iterator+1))
 
-    execute_current_step 3 ${log_file}
+    execute_current_step ${step_iterator} ${log_file}
+    step_iterator=$((step_iterator+1))
 
     echo ":: You should remove or adapt the file >>${log_file}<<."
 }
