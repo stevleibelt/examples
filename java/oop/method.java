51bc98f61959a5cce8962795424d6d00c42bc3e5
Stev Leibelt <stev.leibelt@hrz.tu-freiberg.de>
2023-01-16 10:37:45 +0100

    updated

---
+++ java/oop/method.java
@@ -0,0 +1,26 @@
+package net.bazzline.java.example.loop;
+
+/**
+ * @see: https://opensource.com/article/23/1/java-methods
+ */
+public class Example {
+  //If a class has a main method, java will execute this on file call
+  public static void main(String[] args) {
+
+      long currentTimeMillis = System.currentTimeMillis();
+      string currentStatus;
+
+      if ( currentTimeMillis % 2 == 0 ) {
+          currentStatus = "good";
+      } else {
+          currentStatus = "bad";
+      }
+
+      outputStatus(currentStatus);
+  }
+
+  public static void outputStatus(String status) {
+      System.out.printf(":: Current status is >>%s<<.%n", status);
+  }
+}
+
