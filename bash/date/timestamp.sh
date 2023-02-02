ec10172e504a6ed10161b7b39ad06002d07e159b
stevleibelt <artodeto@bazzline.net>
2023-02-02 22:58:01 +0100

    updated

--- bash/date/timestamp.sh
+++ bash/date/timestamp.sh
@@ -6,14 +6,24 @@
 # @since 2015-03-26
 ########
 
-echo ":: From timestamp to date"
-TIMESTAMP=424044000
-echo "date -d @'${TIMESTAMP}' +'%Y-%m-%d %H:%M:%S'"
-echo $(date -d @${TIMESTAMP} +'%Y-%m-%d %H:%M:%S')
-echo ""
+function _main ()
+{
+  local DATE="06/10/1983"
+  local TIMESTAMP=424044000
 
-echo ":: From date to timestamp"
-DATE='06/10/1983'
-echo "date -d '${DATE}' +'%s'"
-echo $(date -d ${DATE} +'%s')
-echo ""
+  local DATE_FROM_TIMESTAMP=$(date -d @${TIMESTAMP} +'%Y-%m-%d %H:%M:%S')
+  local TIMESTAMP_FROM_DATE=$(date -d ${DATE} +'%s')
+
+  echo ":: From timestamp to date"
+  echo "   date -d @'${TIMESTAMP}' +'%Y-%m-%d %H:%M:%S'"
+  echo "   >>${DATE_FROM_TIMESTAMP}<<"
+  echo ""
+
+  echo ":: From date to timestamp"
+  echo "   date -d '${DATE}' +'%s'"
+  echo "   >>${TIMESTAMP_FROM_DATE}<<"
+  echo ""
+}
+
+_main ${@}
+
