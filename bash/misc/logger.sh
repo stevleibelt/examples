5bd369eb5b2ade99946783cdd9b819f59b4513a7
stevleibelt <artodeto@bazzline.net>
2023-02-02 23:02:26 +0100

    fixed

--- bash/misc/logger.sh
+++ bash/misc/logger.sh
@@ -8,11 +8,11 @@
 
 function _main ()
 {
-    LOCAL_DATE=$(date +'%Y-%m-%d %H:%M:%S')
-    LOCAL_SCRIPT_PATH=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
-    LOCAL_IP=$(hostname -i)
+    local LOCAL_DATE=$(date +'%Y-%m-%d %H:%M:%S')
+    local LOCAL_SCRIPT_PATH=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
+    local LOCAL_IP=$(hostname -i)
 
-    LOCAL_LOG_MESSAGE="[$LOCAL_DATE]: $HOSTNAME - $LOCAL_IP"
+    local LOCAL_LOG_MESSAGE="[$LOCAL_DATE]: $HOSTNAME - $LOCAL_IP"
 
     # either output it
     echo ${LOCAL_LOG_MESSAGE}
