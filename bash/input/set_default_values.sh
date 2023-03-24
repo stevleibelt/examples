ab6b662567fa109ac1dcbadb62a274676fef4045
stevleibelt <artodeto@bazzline.net>
2023-03-24 10:23:48 +0100

    mad them executable

--- bash/input/set_default_values.sh
+++ bash/input/set_default_values.sh
@@ -9,10 +9,18 @@ function _example()
     local VAR=${1:-'foo'}
     local FOO=${2:-''}
     local BAR=${3:-3}
+    local BAZ=${4}
 
     echo "\$VAR >>${VAR}<<."
     echo "\$FOO >>${FOO}<<."
     echo "\$BAR >>${BAR}<<."
+
+    if [[ -z "${BAZ}" ]];
+    then
+      echo "\$BAZ is not set"
+    else
+      echo "\$BAZ >>${BAZ}<<."
+    fi
 }
 
 _example ${@}
