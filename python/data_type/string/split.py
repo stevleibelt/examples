dca5c4aac177b820e410d263af96dc71dc4fc93f
stevleibelt <artodeto@bazzline.net>
2023-05-07 21:56:07 +0200

    Add string section including split example

---
+++ python/data_type/string/split.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+####
+# @see https://www.freecodecamp.org/news/how-to-parse-a-string-in-python/
+# @since 2023-05-07
+# @author stev leibelt <artodeto@bazzline.net>
+####
+
+username = "=+---Doe---+="
+
+# strip will remove all provided characters from the string
+user = username.strip("=+-")
+
+print("username: {}".format(username))
+print("user: {}".format(user))
+
