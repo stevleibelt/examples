7a4e3e24a21ae5779d5a7e625d72035918ef31c9
stevleibelt <artodeto@bazzline.net>
2023-05-14 22:12:10 +0200

    Update Dictionary example with merge options

--- python/data_type/array/dictonary.py
+++ python/data_type/array/dictonary.py
@@ -2,7 +2,9 @@
 ####
 # It is best to think of a dictionary as a set of key: value pairs, with the requirement that the keys are unique (within one dictionary). [...]
 #   The main operations on a dictionary are storing a value with some key and extracting the value given the key.
-# ref: https://docs.python.org/3/tutorial/datastructures.html#dictionaries - 20230403T08:58:10
+# ref:
+#   https://docs.python.org/3/tutorial/datastructures.html#dictionaries - 20230403
+#   https://www.freecodecamp.org/news/python-merge-dictionaries-merging-two-dicts-in-python/ - 20230514
 ####
 # @since 2023-04-03
 # @author stev leibelt <artodeto@bazzline.net>
@@ -40,4 +42,19 @@ if "foo" in exampleDictionary:
     print("   >>exampleDictionary<< does contain >>foo<<.")
 else:
     print("   >>exampleDictionary<< does not contain >>foo<<.")
+
+# Merge two diretories
+# firstDictionary.update(secondDictionary)
+#   Result: first one gets updated
+# `**` unpacks a dictionary
+#   Unpack and merge the key and value pairs
+# thirdDictionary = { **firstDictionary, **secondDictionary }
+#from itertools import chain
+# thirdDictionary = dict(chain(firstDictionary.items(), secondDictionary.items()))
+#
+#from collections import ChainMap
+# thirdDictionary = dict(ChainMap(firstDictionary, secondDictionary))
+# available since python 3.9
+# thirdDictionary = firstDictionary | secondDictionary
+# firstDictionary |= secondDictionary
 
