c1a87bdeed11df4a76e0b1162e917baf052932c2
stevleibelt <artodeto@bazzline.net>
2023-06-11 00:19:51 +0200

    Add function

--- bash/filesystem/file/read_file.sh
+++ bash/filesystem/file/read_file.sh
@@ -6,18 +6,29 @@
 #   http://stackoverflow.com/questions/10929453/read-a-file-line-by-line-assigning-the-value-to-a-variable#10929955
 ###
 
-FILENAME=$(basename $0)
+function _main()
+{
+  local CURRENT_LINE_CONTENT
+  local CURRENT_LINE_NUMBER
+  local FILENAME
 
-CURRENT_LINE_NUMBER=0
-echo -e "line\t: line"
-echo -e "number\t: content"
-echo "--------:--------"
+  FILENAME=$(basename $0)
 
-while read -r CURRENT_LINE_CONTENT;
-do
+  CURRENT_LINE_NUMBER=0
+  echo -e "line\t: line"
+  echo -e "number\t: content"
+  echo "--------:--------"
+
+  while IFS='' read -r CURRENT_LINE_CONTENT || [[ -n "${CURRENT_LINE_CONTENT}" ]];
+  do
+    # filter out empty lines and lines starting with #
+    # if [[ "${#CURRENT_LINE_CONTENT}" -gt 0 ]] && [[ "${CURRENT_LINE_CONTENT:0:1}" != "#" ]];
     echo -e "${CURRENT_LINE_NUMBER}\t: ${CURRENT_LINE_CONTENT}"
     ((++CURRENT_LINE_NUMBER))
-done < "${FILENAME}"
+  done < "${FILENAME}"
 
-#available with bash 4
-readarray -t FILE_CONTENT_AS_ARRAY < "${FILENAME}"
+  #available with bash 4
+  readarray -t FILE_CONTENT_AS_ARRAY < "${FILENAME}"
+}
+
+_main "${@}"
