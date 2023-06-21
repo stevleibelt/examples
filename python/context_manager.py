f0ddae76ed9a745bf1c0312b0b1f8a5c26e2524c
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2023-06-21 10:41:18 +0200

    Add not finished context_manager

---
+++ python/context_manager.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+####
+# ref:
+#   https://fastapi.tiangolo.com/tutorial/dependencies/dependencies-with-yield/
+#   https://docs.python.org/3/library/contextlib.html
+# @since: 2023-06-21
+# @author: stev leibelt <artodeto@bazzline.net>
+####
+
+from contextlib import ContextDecorator
+
+class DBSession:
+    def __init__(self):
+        print(f"Connecting to database")
+
+
+    def query(self, q: str):
+        print(f"Executing query: {q}")
+
+
+    def close(self):
+        print(f"Disconnecting from database")
+
+
+class MyContextManager(ContextDecorator):
+    def __init__(self):
+        print(f"Creating DBSession")
+        self.db = DBSession()
+
+
+    def __enter__(self):
+        print(f"Returning DBSession")
+        return self.db
+
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        print(f"Closing DBSession")
+        self.db.close()
+
+
+async def get_db():
+    with MyContextManager() as db:
+        yield db
+
+
+db = get_db()
+# sadly not working
+#db.query(q="   Doing something while database connection is established.")
