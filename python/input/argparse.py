f24c0b2d6cad738d0031c9f3bb02b32348b940f4
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2023-09-05 11:53:36 +0200

    Update argparser

--- python/input/argparse.py
+++ python/input/argparse.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 ####
 # ref: https://docs.python.org/3/library/argparse.html
 # @author stev leibelt <artodeto.bazzline.net>
@@ -5,33 +6,31 @@
 ####
 
 import argparse
-import os
-import sys
 
 
 def main() -> None:
-    argument_parser = argparse.ArgumentParser(prog="argparse",
-                                              description="Example for using argparse")
+    # ref: https://docs.python.org/3.9/library/argparse.html
+    argument_parser = argparse.ArgumentParser(
+        description='My description',
+        prog="Programmname"
+    )
+    argument_parser.add_argument('-d', '--dry-run', action='store_true', help='Fetch data but do not send')
+    argument_parser.add_argument('-f', '--filename', type=argparse.FileType('r'), help='Optional filename')
+    argument_parser.add_argument('-v', '--verbose', action='store_true', help='Be verbose')
+
+    arguments = argument_parser.parse_args()
+
+    if arguments.filename:
+        print(f'Optional filename provided: {arguments.filename}')
     
-    argument_parser.add_argument("-g", "--givenname", dest="givenname", help="User givenname", type=str)
-    argument_parser.add_argument("-s", "--surname", dest="surname", help="User surname", type=str)
-    argument_parser.add_argument("-t", "--title", dest="title", help="Optional title like doctor", type=str)
+    if arguments.dry_run:
+        print('Dry run enabled')
+    
+    if arguments.verbose:
+        print('Verbosity enabled')
 
-    (args) = argument_parser.parse_args()
-
-    if (args.givenname is None or args.surname is None):
-        print(":: Error")
-        print("   Invalid amount of aruguments provided.")
-        argument_parser.print_help()
-        return 10
-
-    if args.title is None:
-        args.title = ""
-    else:
-        args.title += " "
-
-    print(f"Hello {args.title}{args.givenname} {args.surname}")
-
+    print('Dumping arguments')
+    print(arguments)
 
 
 if __name__ == '__main__':
