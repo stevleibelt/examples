d0cba8a28b27355c73e20e88924f77d0cc175a60
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2023-11-27 10:14:33 +0100

    Remove cache files

--- python.d/__init__.py.d/foo_d/__init__.py
+++
@@ -1,6 +0,0 @@
-print(f'{__file__} is imported')
-
-#this file is always called when the path >>foo_d<< is imported
-
-from foo_d.bar import Bar
-from foo_d.baz import Baz
--- python.d/__init__.py.d/foo_d/baz.py
+++
@@ -1,8 +0,0 @@
-
-print(":: Importing baz.py")
-
-class Baz(object):
-
-    def __init__(self):
-        print(f"{__name__} init is called")
-
