a64ceeaa3a94d713b8685b3540ed0ea790706fcc
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2023-11-28 15:13:40 +0100

    Add exception grouping, else, pass and finally code block

--- python.d/exception.py
+++ python.d/exception.py
@@ -1,11 +1,26 @@
+# ref: https://note.nkmk.me/en/python-try-except-else-finally/
+
 class ExampleException(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
 
+class SecondException(ExampleException):
+    pass
+
+class ThirdException(ExampleException):
+    pass
+
 try:
     print('This is a message')
     raise ExampleException('This is an example exception message')
-except ExampleException as exception:
+except (ExampleException, SecondException) as exception:
     print(f'An exception of class ExampleException occurred: {exception}')
+except ThirdException:
+    # We know this exception could be raised but we don't care
+    pass
+else:
+    print('No exception where raised')
+finally:
+    print('This is the last line of output')
 
