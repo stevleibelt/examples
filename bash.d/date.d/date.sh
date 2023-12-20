f484617fb09c34dd823a13befb6289f4e87a57e8
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2023-12-20 09:51:54 +0100

    Add date example how to go back in time for multiple days

--- bash.d/date.d/date.sh
+++ bash.d/date.d/date.sh
@@ -15,17 +15,20 @@
 #   second: 00
 
 #10/06/83
+echo ":: Create date in us american date format"
 echo "date +'%D'"
 date +'%D'
 echo ""
 
 #13:37:00
+echo ":: Create time only"
 echo "date +'%T'"
 date +'%T'
 echo ""
 
 #13+37
-echo "date +'%H-%M'"
+echo ":: List of Examples for different separators and formats"
+echo "date +'%H+%M'"
 date +'%H+%M'
 echo ""
 
@@ -53,7 +56,13 @@ DATE=$(date +'%Y%m%d-%H%M%S')
 echo ${DATE}
 echo ""
 
+echo ":: Calculating dates"
 echo "date --date=yesterday +%Y%m%d"
 DATE=$(date --date=yesterday +%Y%m%d)
+echo ${DATE}
+echo ""
+
+echo "date --date='-7 day' +%Y%m%d"
+DATE=$(date --date='-7 day' +%Y%m%d)
 echo ${DATE}
 echo ""
