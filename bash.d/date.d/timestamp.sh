ab5363041c7fb937add38b1bf3f566c096a3b45a
stevleibelt <artodeto@bazzline.net>
2024-01-23 21:45:28 +0100

    Add example for datetime to timestamp

--- bash.d/date.d/timestamp.sh
+++ bash.d/date.d/timestamp.sh
@@ -8,11 +8,20 @@
 
 function _main ()
 {
-  local DATE="06/10/1983"
-  local TIMESTAMP=424044000
+  local DATE
+  local DATETIME
+  local TIMESTAMP
+  local DATE_FROM_TIMESTAMP
+  local TIMESTAMP_FROM_DATE
+  local TIMESTAMP_FROM_DATETIME
 
-  local DATE_FROM_TIMESTAMP=$(date -d @${TIMESTAMP} +'%Y-%m-%d %H:%M:%S')
-  local TIMESTAMP_FROM_DATE=$(date -d ${DATE} +'%s')
+  DATE="06/10/1983"
+  DATETIME="1983-10-06 08:15:42"
+  TIMESTAMP=424044000
+
+  DATE_FROM_TIMESTAMP=$(date -d @${TIMESTAMP} +'%Y-%m-%d %H:%M:%S')
+  TIMESTAMP_FROM_DATE=$(date -d ${DATE} +'%s')
+  TIMESTAMP_FROM_DATETIME=$(date -d "${DATETIME}" +'%s')
 
   echo ":: From timestamp to date"
   echo "   date -d @'${TIMESTAMP}' +'%Y-%m-%d %H:%M:%S'"
@@ -23,7 +32,13 @@ function _main ()
   echo "   date -d '${DATE}' +'%s'"
   echo "   >>${TIMESTAMP_FROM_DATE}<<"
   echo ""
+
+  echo ":: From date time to timestamp"
+  echo "   date -d '${DATETIME}' +'%s'"
+  echo "   Should be 434272542"
+  echo "   >>${TIMESTAMP_FROM_DATETIME}<<"
+  echo ""
 }
 
-_main ${@}
+_main "${@}"
 
