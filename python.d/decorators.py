ec549bba18f7b66e91df5640f71438a1d6ae1cc9
stevleibelt <artodeto@bazzline.net>
2024-05-06 21:18:54 +0200

    Extend decorator example with *args, *kwargs and return example

--- python.d/decorators.py
+++ python.d/decorators.py
@@ -1,4 +1,5 @@
 # ref: https://www.freecodecamp.org/news/the-python-decorator-handbook/
+# ref: https://www.freecodecamp.org/news/the-python-decorator-handbook/
 ####
 
 def my_decorator(func):
@@ -10,24 +11,33 @@ def my_decorator(func):
 #   inner functions are called automatically when the outer functions
 #       are called
 #   inner functions are not directly callable.
-    def wrapper():
-        print("Before the function call") # Extra processing before the function
-        func() # Call the actual function being decorated
-        print("After the function call") # Extra processing after the function
+    def wrapper(*args, **kwargs):
+        # Extra processing before the function
+        print(f"Calling {func.__name__} with args: {args} and kwargs: {kwargs}")
+        # Call the actual function being decorated
+        result = func(*args, **kwargs)
+        # Extra processing after the function
+        print(f"Calling {func.__name__} created the result: {result}")
+
+        return result
     return wrapper # Return the nested wrapper function
 
-def my_function():
+def my_function(*args, **kwargs):
     print("Inside my function")
-
-print(":: Calling not decorated function")
-my_function()
-print("")
+    
+    return "This is my result"
 
 # decorated function/function with applied decorator function
 @my_decorator
-def my_function():
-    print("Inside my function")
+def my_other_function(*args, **kwargs):
+    print("Inside my other function")
 
+    return "This is my other result"
+
+
+print(":: Calling not decorated function")
+my_function(1, 2, 3, foo='bar')
+print("")
 print(":: Calling decorated function")
-my_function()
+my_other_function(1, 2, 3, foo='bar')
 
