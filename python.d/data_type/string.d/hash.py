9b420c3797316ec4754fa775b722d50b92def2db
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2025-01-14 11:12:14 +0100

    Add python hash example

---
+++ python.d/data_type/string.d/hash.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+####
+# @since 2025-01-14
+# @author stev leibelt <artodeto@bazzline.net>
+####
+
+from hashlib import sha3_512
+
+my_string: str = "This is a string"
+
+my_hash: str = sha3_512(my_string.encode('utf-8')).hexdigest()
+
+print(f"{my_string=}: {str(my_hash)=}, {len(my_hash)=}")
+
