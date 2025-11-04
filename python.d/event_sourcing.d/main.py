b4bc59cbf0c35431e88fa16b6eaf01844b3982e7
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2025-11-04 17:26:02 +0100

    Add second part of demonstrating an application

--- python.d/event_sourcing.d/main.py
+++ python.d/event_sourcing.d/main.py
@@ -15,7 +15,9 @@ def tutorial_app_part_one(
     registered_event_name: str = typer.Option("Registered"),
     trick_added_event_name: str = typer.Option("TrickAdded")
 ) -> None:
+    from eventsourcing.application import Application
     from eventsourcing.domain import Aggregate, event
+    from typing import Any
     from uuid import UUID
 
     class Dog(Aggregate):
@@ -27,6 +29,24 @@ def tutorial_app_part_one(
         @event(trick_added_event_name)
         def add_trick(self, trick: str) -> None:
             self.tricks.append(trick)
+
+    class DogSchool(Application[UUID]):
+        def register_dog(self, name: str) -> UUID:
+            dog = Dog(name=name)
+            self.save(dog)
+            return dog.id
+        
+        def add_trick(self, dog_id: UUID, trick: str) -> None:
+            dog: Dog = self.repository.get(dog_id)
+            dog.add_trick(trick=trick)
+            self.save(dog)
+
+        def get_dog_as_dict(self, dog_id: UUID) -> dict[str, Any]:
+            dog: Dog = self.repository.get(dog_id)
+            return {"name": dog.name, "tricks": tuple(dog.tricks)}
+
+    if be_verbose:
+        print(":: Demonstrating a aggregate")
     
     dog = Dog(name=dog_name)
     assert isinstance(dog, Dog)
@@ -66,6 +86,36 @@ def tutorial_app_part_one(
         print(f"   Final state: {copy=}")
 
     assert copy == dog
+
+    if be_verbose:
+        print("")
+        print(":: Demonstrating an application")
+
+    application = DogSchool()
+
+    dog_id = application.register_dog(name=dog_name)
+    if be_verbose:
+        print(f"   Registered {dog_id=}")
+
+    application.add_trick(dog_id=dog_id, trick=added_trick_name)
+
+    dog_as_dict = application.get_dog_as_dict(dog_id=dog_id)
+
+    if be_verbose:
+        print(f"   {dog_as_dict=}")
+
+    assert dog_as_dict["name"] == dog_name
+    assert dog_as_dict["tricks"] == (added_trick_name, )
+
+    notifications = application.notification_log.select(start=1, limit=10)
+
+    assert len(notifications) == 2
+    assert notifications[0].id == 1
+    assert notifications[1].id == 2
+
+    if be_verbose:
+        for notification in notifications:
+            print(f"   {notification=}")
 
 
 if __name__ == "__main__":
