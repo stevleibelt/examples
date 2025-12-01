1565b8fda07afc0d242effa8750f5cd9cc9e5b6c
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2025-12-01 10:40:54 +0100

    STYLE

--- bash.d/filesystem.d/file.d/get_file_path.sh
+++ bash.d/filesystem.d/file.d/get_file_path.sh
@@ -4,16 +4,21 @@
 # @since 2014-03-05
 ####
 
-echo 'please provide file name'
-read FILENAME
+function _main() {
+  echo ":: Please insert a file name"
+  read -r FILENAME
 
-FILE_REALPATH=$(realpath $FILENAME);
+  FILE_REALPATH=$(realpath "${FILENAME}");
 
-echo 'provided filename: "'$FILENAME'"'
-echo 'realpath: "'$FILE_REALPATH'"'
+  echo "   Provided filename: >>${FILENAME}<<"
+  echo "   Realpath: >>${FILE_REALPATH}<<"
 
-if [ ! -f "$FILE_REALPATH" ]; then
-    echo 'file does not exist'
-else
-    echo 'file exist'
-fi
+  if [[ ! -f "${FILE_REALPATH}" ]];
+  then
+      echo "   File does not exist"
+  else
+      echo "   File exist"
+  fi
+}
+
+_main "${@}"
