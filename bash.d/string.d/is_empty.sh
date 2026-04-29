e3842cbd0456f5eb897ce09ca8ea0445255df1a0
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2026-04-29 16:33:29 +0200

    Style

--- bash.d/string.d/is_empty.sh
+++ bash.d/string.d/is_empty.sh
@@ -11,36 +11,37 @@
 
 function string_is_empty ()
 {
-    if [[ "${#1}" -eq 0 ]];
-    then
-        return 0
-    else
-        return 1
-    fi
+  # Does also work to check if a variable is set
+  if [[ "${#1}" -eq 0 ]];
+  then
+      return 0
+  else
+      return 1
+  fi
 }
 
 function _main ()
 {
-    if string_is_empty 
-    then
-        echo ":: No value is empty"
-    else
-        echo ":: No value is not empty"
-    fi
+  if string_is_empty 
+  then
+      echo ":: No value is empty"
+  else
+      echo ":: No value is not empty"
+  fi
 
-    if string_is_empty ""
-    then
-        echo ":: \"\" is empty string"
-    else
-        echo ":: \"\" is not empty"
-    fi
+  if string_is_empty ""
+  then
+      echo ":: \"\" is empty string"
+  else
+      echo ":: \"\" is not empty"
+  fi
 
-    if string_is_empty "bazzline"
-    then
-        echo ":: \"bazzline\" is empty"
-    else
-        echo ":: \"bazzline\" is not string"
-    fi
+  if string_is_empty "bazzline"
+  then
+      echo ":: \"bazzline\" is empty"
+  else
+      echo ":: \"bazzline\" is not string"
+  fi
 }
 
 _main
