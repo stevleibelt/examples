53877edf1dc6ed247330627e2a43889caec8e2b2
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2026-05-19 10:34:35 +0200

    Refactor changelog example

--- python.d/changelog.py
+++ python.d/changelog.py
@@ -1,29 +1,38 @@
 ####
-# Simple changelog example done in pure python
+# Simple change log example done in pure python
 ####
 # @since: 2025-11-21
 # @author: stev leibelt <artodeto@bazzline.net>
 ####
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any
 
-@dataclass
+@dataclass(frozen=True)
 class Change:
     current: Any
     previous: Any
 
 
-class Changelog:
+@dataclass(frozen=True)
+class ChangeLog:
     # Extend this dict to support event names or all changes done on one
     #   attribute
-    changes: dict[str, Change]
+    _changes: dict[str, Change] = field(default_factory=dict)
 
-    def __init__(self, changes: dict[str, Change] | None = None):
-        self.changes = changes if changes else {}
 
     def __len__(self) -> int:
-        return len(self.changes)
+        return len(self._changes)
+
+
+
+    def get_change(self, attribute_name: str) -> Change | None:
+        return (
+            self.change_log.changes[attribute_name]
+            if attribute_name in self.change_log.changes
+            else None
+        )
+
 
     def record_change(self, attribute_name: str, new_value: Any, obj: object):
         previous_value = (
@@ -31,36 +40,53 @@ class Changelog:
         )
 
         if previous_value != new_value:
-            # Only last change will be recorded
-            self.changes[attribute_name] = Change(
+            # Only last change will be recorded, existing one will be overwritten
+            self._changes[attribute_name] = Change(
                 current=new_value, previous=previous_value
             )
 
 
 class MyObject:
-    bar: int | None
-    changelog: Changelog
-    foo: str | None
+    _bar: int | None
+    _change_log: ChangeLog
+    _foo: str | None
 
-    def __init__(self, bar: int | None = None, changelog: Changelog | None = None, foo: str | None = None):
-        self.bar = bar
-        self.changelog = changelog if changelog is not None else Changelog()
-        self.foo = foo
+    def __init__(self, bar: int | None = None, change_log: ChangeLog | None = None, foo: str | None = None):
+        self._change_log = change_log if change_log is not None else ChangeLog()
+        self._bar = bar
+        self._foo = foo
 
 
-    def add_change(self, name: str, value: Any) -> None:
+    @property
+    def bar(self) -> int | None:
+        return self._bar
+
+
+    @bar.setter
+    def bar(self, value: int) -> None:
+        self._add_change_and_set_value(name="_bar", value=value)
+
+
+    @property
+    def change_log(self) -> ChangeLog:
+        return self._change_log
+
+
+    @property
+    def foo(self) -> str | None:
+        return self._foo
+
+
+    @foo.setter
+    def foo(self, value: str) -> None:
+        self._add_change_and_set_value(name="_foo", value=value)
+
+
+    def _add_change_and_set_value(self, name: str, value: Any) -> None:
         # This could be a place to implement value [type] validation
         #   or extend MyObject from pydantic.BaseModel.
-        self.changelog.record_change(attribute_name=name, new_value=value, obj=self)
+        self.change_log.record_change(attribute_name=name, new_value=value, obj=self)
         super().__setattr__(name, value)
-
-
-    def get_change(self, attribute_name: str) -> Change | None:
-        return (
-            self.changelog.changes[attribute_name]
-            if attribute_name in self.changelog.changes
-            else None
-        )
 
 
 def main() -> None:
@@ -68,21 +94,22 @@ def main() -> None:
     my_second_object = MyObject(bar=3, foo="foz")
 
     # Making some changes
-    my_first_object.add_change(name="bar", value=1)
-    my_first_object.add_change(name="bar", value=2)
-    my_first_object.add_change(name="foo", value="faz")
+    my_first_object.bar=1
+    my_first_object.bar=2
+    my_first_object.foo="faz"
 
-    my_second_object.add_change(name="foo", value="fiz")
+    my_second_object.foo="fiz"
 
     # Make no changes
-    my_second_object.add_change(name="bar", value=3)
+    my_second_object.bar=3
 
     # Adding a non existing attribute would result into an error
-    # my_second_object.add_change(name="baz", value="there is no foo without a bar")
+    my_second_object.baz="there is no foo without a bar"
 
     # Output
-    print(f"{my_first_object.changelog.changes=}")
-    print(f"{my_second_object.changelog.changes=}")
+    print(f"{my_first_object.change_log=}")
+    print(f"{my_second_object.change_log=}")
+
 
 if __name__ == "__main__":
     main()
